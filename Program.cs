978ff045b35710e2664e98c5c41fb188c005d273
Logan King <65763483+loganator956@users.noreply.github.com>
2022-12-12 16:30:55 +0000

    create basic app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+string homeDir = Path.Combine(Path.GetDirectoryName(Directory.GetCurrentDirectory()), "PortableGit", "home");
+string portableGitDir = Path.Combine(Path.GetDirectoryName(Directory.GetCurrentDirectory()), "PortableGit");
+if (!Directory.Exists(homeDir))
+    throw new DirectoryNotFoundException($"Can't find dir at {homeDir}");
+ProcessStartInfo codeProcess = new ProcessStartInfo(Path.Combine(Directory.GetCurrentDirectory(), "Code.exe"));
+codeProcess.EnvironmentVariables["HOME"] = homeDir;
+codeProcess.EnvironmentVariables["PATH"] = Path.Combine(homeDir, "bin") +
+    ";" + Directory.GetCurrentDirectory() +
+    ";" + portableGitDir;
+Process.Start(codeProcess);
